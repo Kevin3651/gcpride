5dfed445e2848f1e0a13f424f49b648ed151ebe3
Kevin3651 <kevin@charettefamily.com>
2013-07-28 10:17:27 -0700

    User kinda works

--- Gemfile
+++ Gemfile
@@ -3,9 +3,12 @@ source 'https://rubygems.org'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development do
   gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
 end
 
 gem 'sass-rails', '4.0.0'
@@ -17,4 +20,9 @@ gem 'jbuilder', '1.0.2'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
 Gcpride::Application.config.secret_key_base = '23182c0c33577ad4f0e5698b839132614c3b6189ab9f6d9bab1f5c385dc764dcda4614e4f00817db17152899d1a416501114899a12cb1fa8f26ef715fd72246b'
